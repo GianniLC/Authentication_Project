eb2cc1060bd3a2e4e706ef27f209984cce9ec8a3
GianniLC <70944324+GianniLC@users.noreply.github.com>
2022-06-16 11:08:22 +0200

    Revert "Fixing my mistakes... classic"
    This reverts commit efc8004a1970f43db04692db10f63c7bad5c7f17.

--- Authentication/Pages/HRManager.cshtml.cs
+++ Authentication/Pages/HRManager.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Authentication.Pages
@@ -8,7 +9,6 @@ namespace Authentication.Pages
     {
         public void OnGet()
         {
-
         }
     }
 }
